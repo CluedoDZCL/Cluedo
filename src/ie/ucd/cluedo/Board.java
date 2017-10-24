1fa7f2fdcf94ce1cd3ef73d3d5b1c2486a1c1b24
Dan__ <dan.zhang@ucdconnect.ie>
2017-10-24 16:03:39 +0100

    build the Pawn objects

--- src/ie/ucd/cluedo/Board.java
+++ src/ie/ucd/cluedo/Board.java
@@ -41,6 +41,20 @@ public class Board {
 		WeaponCard revolver = new  WeaponCard("revolver");
 		WeaponCard rope = new WeaponCard("rope");
 		WeaponCard poison = new WeaponCard("poison");
+		
+		Pawn candlestick_pawn = new WeaponPawn("candlestick_pawn", 1);
+		Pawn knife_pawn = new WeaponPawn("knife_pawn", 2);
+		Pawn pipe_pawn = new WeaponPawn("lead pipe_pawn", 3);
+		Pawn revolver_pawn = new  WeaponPawn("revolver_pawn",4);
+		Pawn rope_pawn = new WeaponPawn("rope_pawn",5);
+		Pawn poison_pawn = new WeaponPawn("poison_pawn",6);
+		
+		Pawn scarlett_Pawn = new CharPawn("Miss Scarlett_Pawn",1);
+		Pawn plum_Pawn = new CharPawn("Professor Plum_Pawn",2);
+		Pawn peacock_Pawn = new CharPawn("Mrs. Peacock_Pawn",3);
+		Pawn green_Pawn = new CharPawn("Reverend Green_Pawn",4);
+		Pawn mustard_Pawn = new CharPawn("Colonel Mustard_Pawn",5);
+		Pawn white_Pawn = new CharPawn("Mrs White_Pawn",6);
 	}
 	
 	
